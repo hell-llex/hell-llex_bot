19a46ba39413722ed2e75713fb6909cd0006ba33
hell-llex <demeshenko333@gmail.com>
2026-06-06 02:54:45 +0300

    fix: prefer package version in app logs

--- src/config/appInfo.js
+++ src/config/appInfo.js
@@ -4,7 +4,7 @@ const require = createRequire(import.meta.url);
 const pkg = require("../../package.json");
 
 export const APP_VERSION =
+    pkg.version ||
     process.env.BOT_VERSION ||
     process.env.npm_package_version ||
-    pkg.version ||
     "0.0.0";
